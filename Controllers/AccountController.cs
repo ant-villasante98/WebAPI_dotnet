7197f13d9170ff5e75c9d578238a068996950c4e
Antonio Villasante Hilares <hilares33v@gmail.com>
2023-03-23 22:18:31 -0300

    ejercicio terminado

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -29,33 +29,62 @@ namespace Primer_proyecto.Controllers
             new User(){ Id= 1, Email = "martin@email.com", Name = "Admin", Password= "Admin"},
             new User(){ Id= 2, Email = "pepe@email.com", Name = "User 1", Password= "pepe"},
         };
-        [HttpPost]
-        public IActionResult GetToken(UserLogins userLogin)
-        {
+        // [HttpPost]
+        // public IActionResult GetToken(UserLogins userLogin)
+        // {
 
-            // Modificar para Acceder desde el contexto  con Linq
+        //     // Modificar para Acceder desde el contexto  con Linq
+        //     try
+        //     {
+        //         var Token = new UserTokens();
+        //         var Valid = Logins.Any(user => user.Name.Equals(userLogin.UserName, StringComparison.OrdinalIgnoreCase));
+
+        //         // var searchUser = await _context.Users.FindAsync(userLogin.UserName);
+
+        //         if (Valid)
+        //         {
+        //             var user = Logins.FirstOrDefault(user => user.Name.Equals(userLogin.UserName, StringComparison.OrdinalIgnoreCase));
+        //             Token = JwtHelpers.GetTokenKey(new UserTokens()
+        //             {
+        //                 UserName = user.Name,
+        //                 EmailId = user.Email,
+        //                 Id = user.Id
+        //             }, _jwtSettings);
+        //         }
+        //         else
+        //         {
+        //             return BadRequest("Wrong Password");
+        //         }
+        //         return Ok(Token);
+        //     }
+        //     catch (Exception ex)
+        //     {
+        //         throw new Exception("GetToken Error ", ex);
+        //     }
+
+        // }
+
+        [HttpPost]
+        public IActionResult GetToken(User user)
+        {
             try
             {
-                var Token = new UserTokens();
-                var Valid = Logins.Any(user => user.Name.Equals(userLogin.UserName, StringComparison.OrdinalIgnoreCase));
+                if (_context.Users == null) return NotFound();
 
-                // var searchUser = await _context.Users.FindAsync(userLogin.UserName);
+                var token = new UserTokens();
 
-                if (Valid)
+                var result = _context.Users.Find(user.Id);
+
+                if (result == null) return BadRequest("Wrong Password");
+
+                token = JwtHelpers.GetTokenKey(new UserTokens
                 {
-                    var user = Logins.FirstOrDefault(user => user.Name.Equals(userLogin.UserName, StringComparison.OrdinalIgnoreCase));
-                    Token = JwtHelpers.GetTokenKey(new UserTokens()
-                    {
-                        UserName = user.Name,
-                        EmailId = user.Email,
-                        Id = user.Id
-                    }, _jwtSettings);
-                }
-                else
-                {
-                    return BadRequest("Wrong Password");
-                }
-                return Ok(Token);
+                    UserName = result.Name,
+                    EmailId = result.Email,
+                    Id = result.Id
+                }, _jwtSettings);
+
+                return Ok(token);
             }
             catch (Exception ex)
             {
@@ -68,7 +97,9 @@ namespace Primer_proyecto.Controllers
         [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Administrator")]
         public IActionResult GetUserList()
         {
-            return Ok(Logins);
+            if (_context.Users == null) return NotFound();
+            var lista = _context.Users.ToList();
+            return Ok(lista);
 
         }
     }
